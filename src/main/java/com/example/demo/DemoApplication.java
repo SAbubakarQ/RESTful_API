f0ebf20b2dd640e8c9cf4a8167570c7298799bfc
Abubakar Qasim <sahibzadaabubakar.b.qasim@jbhunt.com>
2022-06-10 09:53:08 -0500

    Testing

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -18,6 +18,12 @@ public class DemoApplication {
 		System.out.println("Welcome " + userName);
 		System.out.println("Initiating API...");
 
+		System.out.println("");
+		System.out.println("Local Access: http://localhost:8080/greeting");
+		System.out.println("");
+		System.out.println("Local Access with Username: http://localhost:8080/greeting?name=User");
+		System.out.println("\t*NOTE* Where '?name=User' can be changed to desired username such as '?name=Abubakar'");
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
